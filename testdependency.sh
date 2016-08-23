b667cfac2d4dffefaec0764ea4f09df10d862194
costin <costin@localhost.localdomain>
2016-08-23 15:24:03 +0200

    Adding the testprogram and the testdependency.

---
+++ testdependency.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ~/rpmsample/
+mkdir testdependency
+
+# Create the "executable".
+echo '#!/bin/bash' > testdependency/testdependency
+echo 'echo "Hello from testdependency"' >> testdependency/testdependency
+
+# Create the "configuration" file for the executable.
+echo '# Testdependency.conf' > testdependency/testdependency.conf
+
+# Create the final folder structure.
+mkdir testdependency-1.0
+mkdir -p testdependency-1.0/usr/bin
+mkdir -p testdependency-1.0/etc/testdependency
+
+# Copy the "executable" and the "configuration file", setting correct permissions.
+install -m 755 testdependency/testdependency testdependency-1.0/usr/bin/
+install -m 644 testdependency/testdependency.conf testdependency-1.0/etc/testdependency/
+
+
